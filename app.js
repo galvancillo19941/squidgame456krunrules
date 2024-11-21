93fb3fe8eae1470f060662b90079f64fb4f877c2
galvancillo19941 <43786141+galvancillo19941@users.noreply.github.com>
2024-11-21 08:59:45 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+/**
+ * app.js
+ *
+ * To start the server, run: `node app.js`.
+ *
+ * For example:
+ *   => `npm run start`
+ *   => `node app.js`
+ */
+
+const vulkano = require('@vulkano/core');
+
+vulkano();
